761e75ca9f48ca40e2bbe19abb132aaba6f35830
Cameron Schweder <schwedc20@gmail.com>
2024-07-06 21:55:43 -0500

    created test component and homepage

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1>Header: React Router Tutorial</h1>;
+};
+
+export default Header;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import "../styles/HomePage.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="container home-page">
+        <Header />
+        <h2>Home Page</h2>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
