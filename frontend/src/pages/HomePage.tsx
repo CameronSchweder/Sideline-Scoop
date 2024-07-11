f3c7568ae184f066cd9e20745fc68424744b672e
Cameron Schweder <schwedc20@gmail.com>
2024-07-11 13:04:37 -0500

    applied responsive background / centered header contents

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,19 @@
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
+//import "bootstrap/dist/css/bootstrap.css";
 import "../styles/HomePage.css";
 
 const HomePage = () => {
   return (
     <>
-      <div>
-        <Header color="white">Welcome to</Header>
-        <Header color="#d50a0a">Sideline Scoop</Header>
-        <button type="button" className="btn">
-          Get started //Bootstrap button
-        </button>
+      <div className="headerContainer">
+        <h1 className="header-1">Welcome To</h1>
+        <h1 className="header-2">Sideline Scoop</h1>
+        <Link to="/home">
+          <button type="button" className="btn">
+            Get Started
+          </button>
+        </Link>
       </div>
     </>
   );
